98ac8836b9cad61fd931176a911fc8a9a1715420
Esther Cho <Eunsem.64642@gmail.com>
2019-08-29 15:53:32 -0600

    debugged spotify-this function mystery

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var Spotify = require("node-spotify-api");
 var moment = require("moment");
 var axios = require("axios")
 var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var arg2 = process.argv[3];
@@ -29,6 +30,7 @@ function start(command, arg2) {
             break;
 
         case "do-what-it-says":
+            console.log("do what it says")
             doWhatItSays();
             break;
 
@@ -37,31 +39,29 @@ function start(command, arg2) {
     }
 }
 
-var spotify = new Spotify(keys.spotify);
+
 
 function spotifyThisSong(songName) {
     spotify
         .search({
-            type: "track",
-            query: songName
-        })
-        .then(function (response) {
-            var songs = response.tracks.items;
-            for (var a = 0; a < 5; a++) {
-                console.log("song name: " + songs[a].name);
-                console.log("album: " + songs[a].album.name);
-                console.log("preview url : " + songs[a].preview_url);
-                console.log("artist name: " + songs[a].artists[0].name);
-                console.log("--------------------------------");
+                type: "track",
+                query: songName
+            },
+            function (err, response) {
+                var songs = response.tracks.items;
+                for (var a = 0; a < 5; a++) {
+                    console.log("Song name: " + songs[a].name);
+                    console.log("Album: " + songs[a].album.name);
+                    console.log("Preview url : " + songs[a].preview_url);
+                    console.log("Artist name: " + songs[a].artists[0].name);
+                    console.log("--------------------------------");
+                }
             }
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+        )
 }
 
-var concertThis = function (artist) {
-    axios.get("https://rest.bandsintown.com/artist/" + artist + "/events?app_id=codingbootcamp&date").then(function (response) {
+function concertThis(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
         var songsData = response.data;
         if (!songsData.length) {
             console.log("Nothing found for " + artist);
@@ -69,12 +69,12 @@ var concertThis = function (artist) {
         }
         console.log("Upcoming shows for " + artist + ":");
         for (var a = 0; a < songsData.length; a++) {
-            var res = songsData[i];
+            var res = songsData[a];
             console.log("Venue:" + res.venue.name +
                 " " + "in " + " " +
                 res.venue.city +
-                "," +
-                (res.venue.region || dat.venue.country) +
+                " " +
+                (res.venue.region) +
                 " " +
                 moment(res.datetime).format("MM/DD/YYYY"));
             console.log("--------------------------------");
@@ -82,31 +82,32 @@ var concertThis = function (artist) {
     });
 }
 
-var movieThis = function (movieName) {
+function movieThis(movieName) {
     if (movieName === undefined) {
         movieName = "Mr. Nobody";
     }
+    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy").then(function (respsone) {
+        var res = respsone.data;
+        //Title of the movie.
+        console.log("Title: " + res.Title);
+        //Year the movie came out.
+        console.log("Year: " + res.Year);
+        //IMDB Rating of the movie.
+        console.log("Rated: " + res.Rated);
+        //Rotten Tomatoes Rating of the movie.
+        console.log("Rotten Tomatoes Rating: " + res.Ratings[1].Value);
+        //Country where the movie was produced.
+        console.log("Country: " + res.Country);
+        //Language of the movie.
+        console.log("Language: " + res.Language);
+        //Plot of the movie.
+        console.log("Plot: " + res.Plot);
+        //Actors in the movie.
+        console.log("Actors: " + res.Actors);
+        console.log("--------------------------------");
+    });
 }
-axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy").then(function (respsone) {
-    var res = respsone.data;
-    //Title of the movie.
-    console.log("Title: " + res.Title);
-    //Year the movie came out.
-    console.log("Year: " + res.Year);
-    //IMDB Rating of the movie.
-    console.log("Rated: " + res.Rated);
-    //Rotten Tomatoes Rating of the movie.
-    console.log("Rotten Tomatoes Rating: " + res.Ratings[1].Value);
-    //Country where the movie was produced.
-    console.log("Country: " + res.Country);
-    //Language of the movie.
-    console.log("Language: " + res.Language);
-    //Plot of the movie.
-    console.log("Plot: " + res.Plot);
-    //Actors in the movie.
-    console.log("Actors: " + res.Actors);
-    console.log("--------------------------------");
-});
+
 
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (err, data) {
@@ -117,83 +118,3 @@ function doWhatItSays() {
         console.log("--------------------------------");
     })
 }
-doWhatItSays();
-
-// var bandsInTown = {
-//     provider: "bandsintown",
-//     apiKey: "https://rest.bandsintown.com/artist/" + artist + "/events?app_id=codingbootcamp&date=" + date
-// }
-// bandsInTown
-//     .get({
-//         artist: "pentatonix",
-//         date: "01/01/2020"
-//     })
-//     .then(function (response) {
-//             var songs = response.tracks.items;
-//             for (var a = 0; a < 5; a++) {
-//                 console.log("song name: " + songs[a].name);
-//                 console.log("album: " + songs[a].album.name);
-//                 console.log("preview url : " + songs[a].preview_url);
-//                 console.log("artist name: " + songs[a].artists[0].name);
-//                 console.log(response);
-//                 console.log("--------------------------------");
-//             });
-//     }
-// console.log(bandsInTown(artist, date));
-
-
-// var nodeArgs = process.argv;
-// var movieName = "";
-
-// for (var i = 2; i < nodeArgs.length; i++) {
-
-//     if (i > 2 && i < nodeArgs.length) {
-//         movieName = movieName + "+" + nodeArgs[i];
-//     } else {
-//         movieName += nodeArgs[i];
-//     }
-// }
-
-//     var queryUrl = "https://rest.bandsintown.com/artist/" + artist + "/events?app_id=codingbootcamp&date=" + date;
-
-//     axios.get(queryUrl).then(
-//             function (response) {
-//                 console.log("Release Year: " + response.data.Year);
-//             })
-//         .catch(function (error) {
-//             if (error.response) {
-//                 // The request was made and the server responded with a status code
-//                 // that falls out of the range of 2xx
-//                 console.log("---------------Data---------------");
-//                 console.log(error.response.data);
-//                 console.log("---------------Status---------------");
-//                 console.log(error.response.status);
-//                 console.log("---------------Status---------------");
-//                 console.log(error.response.headers);
-//             } else if (error.request) {
-//                 // The request was made but no response was received
-//                 // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//                 console.log(error.request);
-//             } else {
-//                 // Something happened in setting up the request that triggered an Error
-//                 console.log("Error", error.message);
-//             }
-//             console.log(error.config);
-//         });
-// }
-// movieThis();
-// // axios.get(URL).then(function (response) {
-// //     var jsonData = response.data;
-// //     var showData = [
-// //         "Title: " + jsonData.name,
-// //         "Year: " + jsonData.year,
-// //         "IMDB Rating: " + jsonData.rating.imdb,
-// //         "Rotten Tomatoes Rating: " + jsonData.rating.rotten_tomatoes,
-// //         "Country: " + jsonData.country,
-// //         "Language: " + jsonData.language,
-// //         "Plot: " + jsonData.plot,
-// //         "Actors: " + jsonData.actors
-// //     ].join(`\n\n`);
-
-// //     console.log(showData);
-// // });
